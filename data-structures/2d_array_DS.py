b84b608d57438976a421a88b933c423504619376
dilanka-rathnasiri <rdilanka17@gmail.com>
2026-05-11 13:24:04 +0530

    complete data-structures/2d_array_DS.py

---
+++ data-structures/2d_array_DS.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import math
+
+#
+# Complete the 'hourglassSum' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def hourglassSum(arr):
+    # Write your code here
+    max_sum = -math.inf
+    
+    for i in range(4):
+        for j in range(4):
+            a = arr[i][j]
+            b = arr[i][j+1]
+            c = arr[i][j+2]
+            d = arr[i+1][j+1]
+            e = arr[i+2][j]
+            f = arr[i+2][j+1]
+            g = arr[i+2][j+2]
+            
+            tot = a+b+c+d+e+f+g
+            
+            if tot >= max_sum:
+                max_sum = tot
+    
+    return max_sum
+        
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
